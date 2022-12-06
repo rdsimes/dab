8e55771502c4311f20ad8fe8c2345fc59ba061d7
Richard Simes <rdsimes@gmail.com>
2022-12-06 21:36:18 +1300

    prettier updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Board from './components/Board'
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Board from "./components/Board";
 
 function App() {
   return (
